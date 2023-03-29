937694728ebee09685114306dc7b6ed8a18defbb
William Romeu <williambronstrup13@gmail.com>
2023-03-29 14:38:56 -0300

    Responsividade e js
    Melhorando a responsividade e colocando os "..." no final das descrições

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const modal = document.getElementById("menu-mobile")
+const descriptionCard = document.querySelectorAll(".description")
+
+for (let i = 0; i < descriptionCard.length; i++) {
+  if (descriptionCard[i].innerText.length > 70) {
+    let teste = descriptionCard[i].innerText.substring(0, 65).trim()
+
+    descriptionCard[i].innerText = `${teste}...`
+  }
+}
 
 function openModal() {
   modal.showModal()
