dbe4f494034d6ef42a7d1bd31d3c4ca904182c0a
William Romeu <williambronstrup13@gmail.com>
2023-04-03 17:46:06 -0300

    Criando filtros e validações

--- index.js
+++ index.js
@@ -1,32 +1,75 @@
 const modal = document.getElementById("menu-mobile")
 const descriptionCard = document.querySelectorAll(".description")
 const dropItArea = document.querySelectorAll(".dropIt")
+
 const cards = document.querySelectorAll("section div div")
+const cardsTitle = document.querySelectorAll("section div div h4")
+const cardsDescription = document.querySelectorAll("section div div p")
+const cardsTags = document.querySelectorAll("section div div span")
+
 const inputSearch = document.getElementById("inputSearch")
+const buttonFilter = document.getElementById("btnFilter")
+const filter = document.querySelector(".filter")
+const filterItems = document.querySelectorAll(".filter ul li")
+const filtredItems = document.getElementById("filtrosSelecionados")
+
+/* Deixa as áreas do drop "vazias" como o display none */
 
 for (let i = 0; i < dropItArea.length; i++) {
   dropItArea[i].style.display = "none"
 }
 
-document.addEventListener("dragleave", (e) => {
-  console.log(e.target)
+while (filtredItems.hasChildNodes()) {
+  filtredItems.removeChild(filtredItems.firstChild)
+}
 
+document.addEventListener("dragleave", (e) => {
   for (let i = 0; i < dropItArea.length; i++) {
     dropItArea[i].style.display = "block"
   }
 })
 
 document.addEventListener("dragend", (e) => {
-  // console.log(e.target)
-
   for (let i = 0; i < dropItArea.length; i++) {
     dropItArea[i].style.display = "none"
   }
 })
 
+/* Mostra as opções de filtro */
+
+buttonFilter.addEventListener("click", () => {
+  if (filter.hasAttribute("id")) {
+    filter.removeAttribute("id")
+  } else {
+    filter.setAttribute("id", "show")
+  }
+})
+
+/* Escolha da opção de filtro */
+
+for (let i = 0; i < filterItems.length; i++) {
+  filterItems[i].addEventListener("click", (e) => {
+    let filtro = e.target.innerText.toLowerCase()
+
+    let span = document.createElement("span")
+
+    span.innerText = `* ${filtro}`
+    // corrigir erro de validação
+
+    for (let child of filtredItems.children) {
+      console.log(child.innerText)
+    }
+
+    filterItems[i].classList.toggle("itemSelecionado")
+    filtredItems.appendChild(span)
+  })
+}
+
 /* Filtro de pesquisa pelo input */
 
-inputSearch.addEventListener("keyup", (e) => {
+inputSearch.addEventListener("keyup", (e) => search(e))
+
+function search(e) {
   let valorDigitado = e.target.value.toLowerCase()
 
   for (let i = 0; i < cards.length; i++) {
@@ -36,7 +79,7 @@ inputSearch.addEventListener("keyup", (e) => {
       cards[i].style.display = "block"
     }
   }
-})
+}
 
 /* Description Card Length */
 
